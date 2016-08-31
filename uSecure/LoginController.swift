5f5db0b4101d0369f73334527c3d8d7f27ea969c
Kevin Grozav <kevin@oksanas-macbook-pro.local>
2016-08-30 21:48:24 -0700

    Initial Commit

---
+++ uSecure/LoginController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginController.swift
+//  uSecure
+//
+//  Created by Kevin Grozav on 8/30/16.
+//  Copyright © 2016 Kevin Grozav. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
